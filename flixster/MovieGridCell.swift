d98a0ef94853cd5b82c5e01da7895a09c38fe53c
Lucas Sun <gostlucaspp@gmail.com>
2021-02-27 09:47:47 -0500

    part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Lucas Sun on 2/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
